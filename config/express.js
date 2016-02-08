46268c5bfea4ee9b801fd5a15d3da69748424158
ivermac <mark.ekisa@gmail.com>
2016-02-08 16:27:15 +0300

    ME: create custom css variable and loading if it exists

--- config/express.js
+++ config/express.js
@@ -68,6 +68,7 @@ app.use( function( req, res, next ) {
     res.locals.trackingDomain = req.app.get( 'google' ).analytics.domain || 'auto';
     res.locals.logo = req.app.get( 'logo' );
     res.locals.defaultTheme = req.app.get( 'default theme' ).replace( 'theme-', '' ) || 'kobo';
+    res.locals.customCss = req.app.get( 'custom css' );
     res.locals.title = req.app.get( 'app name' );
     res.locals.offline = req.app.get( 'offline enabled' ); // temporary to show 'Experimental' warning
     res.locals.directionality = function() {
